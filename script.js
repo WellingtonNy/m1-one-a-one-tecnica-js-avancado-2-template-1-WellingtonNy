7b9f2430c20f3a6e67ec2104f06abadbbd87390f
Wellington <Wellingtoncpfilho@gmail.com>
2022-08-29 11:21:54 -0300

    entrega1-1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//Desenvolva seu código aqui!
+function verificaLista(lista,numero){
+    let contadorDeNumero = 0
+    for(let i=0; i<lista.length;i++){
+        if(lista[i] == numero){
+            contadorDeNumero++
+        }
+    }
+    if(contadorDeNumero>=2){
+        return "Aparece duas vezes ou mais"
+    }else{
+       return "Aparece menos de duas vezes"
+    }
+}
